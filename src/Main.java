bf1f29dfdb05689b76bc66bcbb4a6f79cc8373ee
Esatjahan <154762403+Esatjahan@users.noreply.github.com>
2024-08-22 16:18:25 +0600

    Project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+//OTP   = One-Time-Pad
+/*
+           encryption technique that can not be cracked ,
+            but requires the use of one time pre shared
+            key the same size as,or longer than,the
+            message being sent.In this technique,plaintext is paired
+            with a random secret key(also referred to as is a one time pad
+            */
+
+        EncryptionProgramme ep=new EncryptionProgramme();
+    }
+}
